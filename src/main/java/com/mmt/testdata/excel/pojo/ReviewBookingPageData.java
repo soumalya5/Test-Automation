ed99d5eb9d0c890ed992e3902f19e76566837ef2
Soumalya Mondal <soumalya.mondal6@gmail.com>
2022-10-17 19:42:43 +0530

    Update ReviewBookingPageData.java

--- src/main/java/com/mmt/testdata/excel/pojo/ReviewBookingPageData.java
+++ src/main/java/com/mmt/testdata/excel/pojo/ReviewBookingPageData.java
@@ -4,11 +4,19 @@ import java.util.HashMap;
 
 import lombok.Getter;
 
-@Getter
+
 public class ReviewBookingPageData {
 
 	private String reviewBooking;
 	
+	public String getReviewBooking() {
+		return reviewBooking;
+	}
+
+	public void setReviewBooking(String reviewBooking) {
+		this.reviewBooking = reviewBooking;
+	}
+
 	public ReviewBookingPageData(HashMap<String, String> data) {
 		reviewBooking = data.get("ReviewBooking");
 	}
