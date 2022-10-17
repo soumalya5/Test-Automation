a59a9e93373fd62c2a72f088244d875b1959f93e
Soumalya Mondal <soumalya.mondal6@gmail.com>
2022-10-17 19:42:49 +0530

    Update HomePageData.java

--- src/main/java/com/mmt/testdata/excel/pojo/HomePageData.java
+++ src/main/java/com/mmt/testdata/excel/pojo/HomePageData.java
@@ -3,11 +3,19 @@ package com.mmt.testdata.excel.pojo;
 import java.util.HashMap;
 import lombok.Getter;
 
-@Getter
+
 public class HomePageData {
 
 	private String departureDate;
 	
+	public String getDepartureDate() {
+		return departureDate;
+	}
+
+	public void setDepartureDate(String departureDate) {
+		this.departureDate = departureDate;
+	}
+
 	public HomePageData(HashMap<String, String> data) {
 		departureDate = data.get("DepartureDate");
 	}
