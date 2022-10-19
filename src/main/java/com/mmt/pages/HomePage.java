4ccc68e1306833586952d31ee763c14523768f47
Soumalya Mondal <soumalya.mondal6@gmail.com>
2022-10-19 23:22:35 +0530

    Update HomePage.java

--- src/main/java/com/mmt/pages/HomePage.java
+++ src/main/java/com/mmt/pages/HomePage.java
@@ -3,7 +3,10 @@ package com.mmt.pages;
 import java.util.HashMap;
 import java.util.List;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+
+import com.mmt.drivers.DriverManager;
 import com.mmt.support.waits.WaitStrategy;
 import com.mmt.testdata.excel.pojo.HomePageData;
 
@@ -19,16 +22,22 @@ public class HomePage extends BasePage {
 	
 	private String srcCityName = "//p[text()='%s, India']";
 	private String destCityName = "//p[text()='%s, India']";
-	
+	private final By POP_UP= By.xpath("//a[@id='webklipper-publisher-widget-container-notification-close-div']");
 	
 	
 	public void selectFlight() {
 		click(BODY, WaitStrategy.NONE);
 		click(LNK_FLIGHTS, WaitStrategy.CLICKABLE);
 	}
+	public void closePopup() {
+		DriverManager.getDriver().switchTo().frame("webklipper-publisher-widget-container-notification-frame");
+		click(POP_UP, WaitStrategy.CLICKABLE);
+		DriverManager.getDriver().switchTo().defaultContent();
+	}
 	
 	
 	public void selectOneWayTrip() {
+		click(BODY,WaitStrategy.NONE);
 		click(RADIO_ONEWAY, WaitStrategy.CLICKABLE);
 	}
 	
@@ -58,7 +67,7 @@ public class HomePage extends BasePage {
 	
 	
 	public void clickOnSearch() {
-		click(BODY, WaitStrategy.NONE);
+		//click(BODY, WaitStrategy.NONE);
 		click(BTN_SEARCH, WaitStrategy.CLICKABLE);		
 	}
 }
