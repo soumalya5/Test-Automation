2b581e26eb6e391f39f964aef597f8ce4b744189
saranyadevi.b <saranyaraj7327@gmail.com>
2022-11-22 17:16:41 +0530

    Edited the report file name to index

--- src/test/java/com/mmt/testrunners/CucumberRunners.java
+++ src/test/java/com/mmt/testrunners/CucumberRunners.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/com/mmt/features",
 					glue = "com/mmt/stepdefinitions",
-					plugin = { "html:src/test/resources/cucumber-reports/report.html" },
+					plugin = { "html:src/test/resources/cucumber-reports/index.html" },
 					monochrome = true)
 public class CucumberRunners {
 
